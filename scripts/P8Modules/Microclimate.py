6ed2d822ff5aca2bf2a1f53401573dd3ac2d61e5
Clemens Degasper <clemens.degasper@gmail.com>
2013-03-07 16:43:24 +0100

    new dummy module, get implemented later

---
+++ scripts/P8Modules/Microclimate.py
@@ -0,0 +1,16 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from pydynamind import *
+
+class Microclimate(Module):
+	def __init__(self):
+	    Module.__init__(self)
+
+	    self.blocks = View("Block",FACE,READ)
+	    self.TableData = View("Table Data",COMPONENT,WRITE)
+	    datastream = []
+	    datastream.append(self.blocks)
+	    datastream.append(self.TableData)
+	    self.addData("City",datastream)
+	def run(self):
+	    city = self.getData("City")
