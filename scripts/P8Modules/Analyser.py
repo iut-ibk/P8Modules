e61e6e96220995ada7580e78486a5154621c2de1
Clemens Degasper <clemens.degasper@gmail.com>
2013-04-04 13:08:06 +0200

    read now attributes from data stream and transfers them to the gui

--- scripts/P8Modules/Analyser.py
+++ scripts/P8Modules/Analyser.py
@@ -7,15 +7,33 @@ class Analyser(Module):
 	def __init__(self):
 	    Module.__init__(self)
 
-	    self.blocks = View("Block",FACE,READ)
+	    self.simulation = View("SimulationData",COMPONENT,READ)
+	    self.simulation.getAttribute("Utilisations")
+	    self.simulation.getAttribute("Runs")
+
 	    self.TableData = View("Table Data",COMPONENT,WRITE)
 	    datastream = []
-	    datastream.append(self.blocks)
+	    datastream.append(self.simulation)
 	    datastream.append(self.TableData)
 	    self.addData("City",datastream)
 	def run(self):
-	    city = self.getData("City")
+		city = self.getData("City")
+		strvec = city.getUUIDsOfComponentsInView(self.simulation)
+		self.Utilvec = []
+		runvec = []
+		for value in strvec:
+			simuAttr = city.getComponent(value)
+			tmpvec = simuAttr.getAttribute("Utilisations").getDoubleVector()
+			run = simuAttr.getAttribute("Runs").getDouble()
+			if (run != 0):
+				runvec.append(int(run))
+			for zahl in tmpvec:
+				self.Utilvec.append(int(zahl))
+		self.runs = runvec[0]
+		print self.Utilvec
+		print self.runs
+
 	def createInputDialog(self):
-            form = Analyser_Gui(self, QApplication.activeWindow())
-            form.show()
-            return True 
+		form = Analyser_Gui(self, QApplication.activeWindow())
+		form.show()
+		return True 
