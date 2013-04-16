872278297d5f5124d6f40b7a5625a3448f884221
Clemens Degasper <clemens.degasper@gmail.com>
2013-04-16 13:21:30 +0200

    module reads number form gui and adds it to the datastream

--- src/p8realisations.cpp
+++ src/p8realisations.cpp
@@ -1,4 +1,8 @@
+
 #include "p8realisations.h"
+#include "p8realisation_gui.h"
+
+#include <QWidget>
 
 DM_DECLARE_NODE_NAME(Realisations,P8Modules)
 
@@ -10,23 +14,30 @@ Realisations::Realisations()
 
 void Realisations::init()
 {
-    this->addPort("in",DM::INPORTS);
-    this->addPort("out",DM::OUTPORTS);
-
+    Block = DM::View("Block",DM::FACE,DM::READ);
 
     Simu = DM::View("SimulationData",DM::COMPONENT,DM::WRITE);
     Simu.addAttribute("MusicFileNo");
     std::vector<DM::View> vdata;
     vdata.push_back(Simu);
+    vdata.push_back(Block);
     this->addData("City",vdata);
 }
 
 void Realisations::run()
 {
+    DM::System * sys = this->getData("City");
+    DM::Component * cmp = new DM::Component();
+    sys->addComponent(cmp,Simu);
 
+    cmp->addAttribute("MusicFileNo",atoi(this->RealisationNr.c_str()));
 }
 
 bool Realisations::createInputDialog()
 {
+    QWidget * w = new p8realisation_gui(this);
+    w->show();
     return true;
 }
+
+
