4a44339d6802906f39d5bb20e19a17c03b9edea0
Clemens Degasper <clemens.degasper@gmail.com>
2013-07-09 16:33:39 +0200

    new button thats read the location of the csv file the labeling of the the x axis is now dynamic [mm/x min] or [mm/x h]

--- scripts/P8Modules/rain_gui.py
+++ scripts/P8Modules/rain_gui.py
@@ -13,9 +13,12 @@ class RainGui(QtGui.QDialog):
         self.ui = Ui_P8Rain_GUI()
         self.ui.setupUi(self)
         self.ui.le_r.setText(self.module.getParameterAsString("FileName"))
+        self.ui.le_csv.setText(self.module.getParameterAsString("csvFile"))
         QtCore.QObject.connect(self.ui.pb_preview, QtCore.SIGNAL("released()"), self.preview)
         QtCore.QObject.connect(self.ui.buttonBox, QtCore.SIGNAL("accepted()"), self.save_values)
         QtCore.QObject.connect(self.ui.pb_r, QtCore.SIGNAL("released()"), self.load)
+        QtCore.QObject.connect(self.ui.pb_csv, QtCore.SIGNAL("released()"), self.loadcsv)
+
         
     def preview(self):
         vec = []
@@ -29,10 +32,13 @@ class RainGui(QtGui.QDialog):
         plt.show()
         a.close()
         '''
-
+        timestep = 0
         f = open("RainData.csv","r")
         for line in f:
             linearr = line.strip('\n').split(',')
+            if (timestep == 0):
+                timestep = int(linearr[0])
+                continue
             tmpbar = round(float(linearr[1]),1)
             if (tmpbar>0.00):
                 if (tmpbar in dic):
@@ -51,19 +57,36 @@ class RainGui(QtGui.QDialog):
         for i in svec:
             count.append(i[1])
             ind.append(i[0])
+        timestep = timestep /60
+        timestepstring = ""
+        if (timestep > 60):
+            timestep = timestep /60
+            timestepstring = str(timestep) + "h"
+        else:
+            timestepstring = str(timestep) + "min"
         bars = ax.bar(ind,count,.1,color='blue',edgecolor='none')
         ax.set_title('Rainfall frequency')
         ax.set_ylabel('Frequency [-]')
-        ax.set_xlabel('Rainfall [mm/h]')
+        ax.set_xlabel('Rainfall [mm/'+timestepstring+']')
         plt.show()
 
     def save_values(self):
+        Net = False
+        Csv = False
         filename = str(self.ui.le_r.text())
-        self.module.setParameterValue("FileName", filename)
-
+        if (filename != ""):
+            Net = True
+            self.module.setParameterValue("FileName", filename)
+        filename = str(self.ui.le_csv.text())
+        if (filename != ""):
+            Csv = True
+            self.module.setParameterValue("UserCsv", str(1))
+            self.module.setParameterValue("csvFile",filename)
+        if (not(Net or Csv)):
+            print "Warning: no File Selected"
     def load(self):
         filename = QtGui.QFileDialog.getOpenFileName(self, "Open rain file", "Open new file", self.tr("Text Files (*.nc)"))
         self.ui.le_r.setText(filename)
-        self.save_values
-
-
+    def loadcsv(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, "Open rain file", "Open new file", self.tr("CSV Files (*.csv)"))
+        self.ui.le_csv.setText(filename)
