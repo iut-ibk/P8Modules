76e1a02208c751287363f5cceeabdb2ef9bfc0d6
Clemens Degasper <clemens.degasper@gmail.com>
2015-11-04 16:16:44 +0100

    re un comment rain.py reusing that old module for testing purposes

--- scripts/P8Modules/__init__.py
+++ scripts/P8Modules/__init__.py
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
-#from rain import *
+from rain import *
 from rain2 import * 
 #from ReadTable import *
 from ReadTable2 import *
